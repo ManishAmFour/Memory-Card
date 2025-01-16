c60060ba61ae9b6dc608b4f1a310b55af5b7ad9e
Manish Tewatia <ManishTewatia3020@gmail.com>
2025-01-17 00:01:06 +0530

    established the basic structure nothing substantial yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    let List = fetch("https://pokeapi.co/api/v2/")
+      .then((result) => {
+        return result.json();
+      })
+      .then((resolved) => {
+        resolved;
+      });
+
+    console.log(List);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="main-div">
+      <div className="static-div">
+        <a className="static-title">Pokemon Memory Card</a>
+        <a className="static-description">
+          get points by clicking on an image but don't click on any more than
+          once!
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
