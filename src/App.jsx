1648b872be2d9463ac07945334feb794ca134f4d
Manish Tewatia <ManishTewatia3020@gmail.com>
2025-01-24 01:02:35 +0530

    implemented the logic successfully and also updated the logic behind score boards. with every things considering only thing which remains is deployement

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import "./App.css";
 import { result, set } from "lodash";
 import { FetchingTheData } from "./ImageArray";
 import { uniqueRandomNumArrayGenerator } from "./uniqueRandomGen";
-import { verificationFunction } from "./verification";
+import { verificationFunction, calculateBestScore } from "./verification";
 
-function ImageInteraction({ array, parentComp }) {
+function ImageInteraction({ array, setScoreComp, setBestScoreComp }) {
   const [ModifiedArray, setArray] = useState(array);
-  const [clickedImages, setClickedImages] = useState(0);
+  const [clickedImages, setClickedImages] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
   function ModifyingTheArray(image) {
     let NewArray = uniqueRandomNumArrayGenerator(ModifiedArray);
     setArray(NewArray);
-    parentComp(false);
-    verificationFunction(image, clickedImages);
+    setCurrentScore(verificationFunction(image, clickedImages));
     setClickedImages(clickedImages);
   }
+
+  useEffect(() => {
+    setScoreComp(currentScore);
+    setBestScoreComp(calculateBestScore(currentScore));
+  }, [ModifiedArray]);
 
   return ModifiedArray.map((image, index) => {
     return (
@@ -34,7 +39,8 @@ function ImageInteraction({ array, parentComp }) {
 
 function App() {
   const [imagesFilledArray, setImagesFilledArray] = useState(false);
-  const [alterTheArray, setAlterTheArray] = useState(true);
+  const [score, setScore] = useState();
+  const [bestScore, setBestScore] = useState(0);
   useEffect(() => {
     FetchingTheData().then((MainArray) => {
       setImagesFilledArray(MainArray);
@@ -45,23 +51,24 @@ function App() {
     <div className="main-div">
       <div className="score-info">
         <a>
-          Score:- <span className="current-score"></span>
+          Score:- <span className="current-score">{score}</span>
         </a>
         <a>
-          Best Score:- <span className="best-score"></span>
+          Best Score:- <span className="best-score">{bestScore}</span>
         </a>
       </div>
       <div className="static-div">
         <a className="static-title">Pokemon Memory Card</a>
 
         <a className="static-description">
-          get points by clicking on an image but don't click on any more than
-          once!
+          get points by clicking on the pokemon image below but don't click on
+          any more than once!
         </a>
         {imagesFilledArray ? (
           <div className="pokemon-container">
             <ImageInteraction
-              parentComp={setAlterTheArray}
+              setScoreComp={setScore}
+              setBestScoreComp={setBestScore}
               array={imagesFilledArray}
             />
           </div>
--- src/verification.jsx
+++ src/verification.jsx
@@ -1,11 +1,27 @@
+let score = 0;
+
 export function verificationFunction(image, clickedImages) {
   let clickedImageArray = clickedImages;
 
   if (clickedImageArray.includes(image)) {
     clickedImageArray.splice(0, clickedImageArray.length);
+    score = 0;
   } else {
     clickedImageArray.push(image);
+    score += 1;
   }
+  return score;
+}
 
-  return clickedImageArray;
+let BestScore = 0;
+
+export function calculateBestScore(currentScore) {
+  if (currentScore === 0) {
+    return BestScore;
+  } else {
+    if (currentScore > BestScore || BestScore === 0) {
+      BestScore += 1;
+    }
+  }
+  return BestScore;
 }
